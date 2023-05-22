75cc4563bc31d32bf02ea9f95caecf36b224a636
nnonne <kmakowetska@gmail.com>
2023-05-23 00:52:53 +0300

    fix1

--- main/draw.py
+++ main/draw.py
@@ -12,23 +12,34 @@ def draw_points(points):
         t.up()
 
 def connect(x1, x2, state):
-    c = "red" if state == "a" else "blue"
+    c = "purple" if state == "a" else "green"
     t.up()
+    t.width(3)
     t.goto(x1, 0)
     t.color(c)
     t.down()
     if x1 > x2:
         t.left(90)
-        t.circle(radius=abs(x2 - x1) / 2, extent=180)
+        t.circle(radius=abs(x2 - x1) / 2, extent=90)
+        t.write(state, font=("Verdana",
+                                    15, "normal"))
+        t.circle(radius=abs(x2 - x1) / 2, extent=90)
         t.left(90)
     elif x1 < x2:
         t.right(90)
-        t.circle(radius=abs(x2 - x1) / 2, extent=180)
+        t.circle(radius=abs(x2 - x1) / 2, extent=90)
+        t.write(state, font=("Verdana",
+                                    15, "normal"))
+        t.circle(radius=abs(x2 - x1) / 2, extent=90)
         t.right(90)
     else:
         t.right(90)
-        t.circle(radius=15, extent=360)
+        t.circle(radius=15, extent=180)
+        t.write(state, font=("Verdana",
+                                    15, "normal"))
+        t.circle(radius=15, extent=180)
         t.left(90)
+
 
 global t
 t = turtle.Turtle()
@@ -38,8 +49,11 @@ p = Points(n)
 draw_points(p)
 for el in p.points:
     na, nb = go(el)
-    print(f"a({el}) -> {na}")
-    print(f"b({el}) -> {nb}")
+    print(f"\lambda(a, {el}) = {na}")
+    print(f"\lambda(b,{el}) = {nb}")
     connect(p.find_point(el), p.find_point(na), "a")
     connect(p.find_point(el), p.find_point(nb), "b")
+
+t.up()
+t.goto(1000,1000)
 turtle.mainloop()
